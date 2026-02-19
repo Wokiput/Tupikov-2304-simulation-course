9ddd1a6ec3144650a25696606d3375a6136974fc
Тупиков Данил <tupikovdtomsk@gmail.com>
2026-02-19 13:34:16 +0700

    cvb

--- lab01/WinFormsApp1/Form1.cs
+++ lab01/WinFormsApp1/Form1.cs
@@ -13,50 +13,51 @@ namespace WinFormsApp1
         }
 
         private double y0, x, y, v0, alpha, sina, cosa, t, S, m, k, vx, vy, vx_new, vy_new, ymax=-1;
-
-        private int SeriesCounter = 0;
-
+        
+        private static int SeriesCounter = 0;
+        private Series newSeries = new Series($"series{SeriesCounter}");
         private const double g = 9.8;
-
         private void btn_Start_Click(object sender, EventArgs e)
         {
-            string data = "=== New Series - " + SeriesCounter + " ===\n"; 
-            string SeriesName = $"series{SeriesCounter}";
-            Series newSeries = new Series(SeriesName);
-            newSeries.Color = Color.FromArgb((SeriesCounter * 60) % 255, (SeriesCounter * 80) % 255, (SeriesCounter * 110) % 255);
-            newSeries.ChartType = SeriesChartType.Line;
-            newSeries.BorderWidth = 2;
-            double dt = (double)cB_dt.SelectedItem;
-            y0 = (double)nUD_height.Value;
-            alpha = (double)nUD_angle.Value;
-            S = (double)nUD_size.Value;
-            m = (double)nUD_weight.Value;
-            v0 = (double)nUD_speed.Value;
-            ymax = 0;
-            data += "y0 = " + y0 + "    alpha = " + alpha + "\nS = " + S + "    m = " + m + "   v0 = " + v0 + "\ndt = " + dt;
-            sina = Math.Sin(alpha * Math.PI / 180);
-            cosa = Math.Cos(alpha * Math.PI / 180);
-            k = 0.15 * S * 1.29 / (2 * m);
-            t = 0; x = 0; y = 0;
-            newSeries.Points.AddXY(x, y);
-            vx_new = v0 * cosa; vy_new = v0 * sina;
-            do
+            if (timer1.Enabled)
             {
-                if (ymax < y) ymax = y;
-                t += dt;
-                vx = vx_new; vy = vy_new;
-                double v = Math.Sqrt(vx * vx + vy * vy);
-                vx_new = vx - k * vx * v * dt;
-                vy_new = vy - (g + k * vy * v) * dt;
-                x += vx * dt;
-                y += vy * dt;
+                string data = "=== New Series - " + SeriesCounter + " ===\n";
+                newSeries.Color = Color.FromArgb((SeriesCounter * 60) % 255, (SeriesCounter * 80) % 255, (SeriesCounter * 110) % 255);
+                newSeries.ChartType = SeriesChartType.Line;
+                newSeries.BorderWidth = 2;
+                double dt = (double)cB_dt.SelectedItem;
+                y0 = (double)nUD_height.Value;
+                alpha = (double)nUD_angle.Value;
+                S = (double)nUD_size.Value;
+                m = (double)nUD_weight.Value;
+                v0 = (double)nUD_speed.Value;
+                ymax = 0;
+                data += "y0 = " + y0 + "    alpha = " + alpha + "\nS = " + S + "    m = " + m + "   v0 = " + v0 + "\ndt = " + dt;
+                sina = Math.Sin(alpha * Math.PI / 180);
+                cosa = Math.Cos(alpha * Math.PI / 180);
+                k = 0.15 * S * 1.29 / (2 * m);
+                t = 0; x = 0; y = 0;
                 newSeries.Points.AddXY(x, y);
-            } while (y > 0);
-            data += "\nx = " + x + "    ymax = " + ymax + " v = " + Math.Sqrt(vx * vx + vy * vy) + "\n";
-            data += new string('=', 30) + "\n\n";
-            chart1.Series.Add(newSeries);
-            SeriesCounter++;
-            File.AppendAllText(@"..\..\..\..\data.txt", data, System.Text.Encoding.UTF8);
+                vx_new = v0 * cosa; vy_new = v0 * sina;
+                timer1.Start();
+                data += "\nx = " + x + "    ymax = " + ymax + " v = " + Math.Sqrt(vx * vx + vy * vy) + "\n";
+                data += new string('=', 30) + "\n\n";
+                chart1.Series.Add(newSeries);
+                SeriesCounter++;
+                File.AppendAllText(@"..\..\..\..\data.txt", data, System.Text.Encoding.UTF8);
+            }
+        }
+        private void timer1.Tick(object sender, EventArgs e)
+        {
+            if (ymax <= y) ymax = y;
+            t += (double)cB_dt.SelectedItem;
+            double v = (double)Math.Sqrt((double)(vx* vx + vy * vy));
+            vx = vx - k * vx * v * (double)cB_dt.SelectedItem;
+            vy = vy - (g + k * vy * v) * (double)cB_dt.SelectedItem;
+            x = x + vx * (double)cB_dt.SelectedItem;
+            y = y + vy * (double)cB_dt.SelectedItem;
+            newSeries.Points.AddXY(x, y);
+            if (y <= 0) timer1.Stop();
         }
         private void InitializePrecisionSelector()
         {
