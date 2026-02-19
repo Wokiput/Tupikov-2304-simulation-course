29edfe1621ec4e587b2a7a6c0220b69470c70b69
GalaxyGiNiTi <hdjfn7807@gmail.com>
2026-02-19 14:24:29 +0700

    bugfix

--- lab01/WinFormsApp1/Form1.cs
+++ lab01/WinFormsApp1/Form1.cs
@@ -15,12 +15,12 @@ namespace WinFormsApp1
         private double y0, x, y, v0, alpha, sina, cosa, t, S, m, k, vx, vy, vx_new, vy_new, ymax;
 
         private static int SeriesCounter = 0;
-        private Series newSeries = new Series($"series{SeriesCounter}");
         private const double g = 9.8;
         private void btn_Start_Click(object sender, EventArgs e)
         {
             if (!timer1.Enabled)
             {
+                newSeries = new Series($"series{SeriesCounter}");
                 string data = "=== New Series - " + SeriesCounter + " ===\n";
                 newSeries.Color = Color.FromArgb((SeriesCounter * 60) % 255, (SeriesCounter * 80) % 255, (SeriesCounter * 110) % 255);
                 newSeries.ChartType = SeriesChartType.Line;
@@ -39,12 +39,11 @@ namespace WinFormsApp1
                 t = 0; x = 0; y = y0;
                 newSeries.Points.AddXY(x, y0);
                 vx_new = v0 * cosa; vy_new = v0 * sina;
+                chart1.Series.Add(newSeries);
                 timer1.Start();
                 data += "\nx = " + x + "    ymax = " + ymax + " v = " + Math.Sqrt(vx * vx + vy * vy) + "\n";
                 data += new string('=', 30) + "\n\n";
                 SeriesCounter++;
-                chart1.Series.Add(newSeries);
-                newSeries = new Series($"series{SeriesCounter}");
                 File.AppendAllText(@"..\..\..\..\data.txt", data, System.Text.Encoding.UTF8);
             }
         }
